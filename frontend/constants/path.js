a7aac211aa303fee86cedb492db3bf865d2a1a3b
Cinderella <pakornkiet2186@gmail.com>
2025-08-07 23:24:25 +0900

    Update path.js

--- frontend/constants/path.js
+++ frontend/constants/path.js
@@ -1,4 +1,4 @@
-
+// http://procs-g02.mwit.ac.th:9091
 const BASE_API_URL = __DEV__ ? "https://e-icon-application-1.onrender.com" : "https://e-icon-application-1.onrender.com";
 
 export default BASE_API_URL;
